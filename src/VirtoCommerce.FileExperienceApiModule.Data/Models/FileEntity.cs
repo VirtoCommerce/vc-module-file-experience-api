e9fb597422b52b7037cf6fb06a443e426d489e09
Basil Kotov <basilkot@gmail.com>
2024-01-08 15:06:38 +0000

    FileEntity inherits AuditableEntity

--- src/VirtoCommerce.FileExperienceApiModule.Data/Models/FileEntity.cs
+++ src/VirtoCommerce.FileExperienceApiModule.Data/Models/FileEntity.cs
@@ -1,8 +1,9 @@
+using VirtoCommerce.Platform.Core.Common;
+
 namespace VirtoCommerce.FileExperienceApiModule.Data.Models
 {
-    public class FileEntity
+    public class FileEntity : AuditableEntity
     {
-        public string Id { get; set; }
         public string Name { get; set; }
         public string MimeType { get; set; }
         public long Size { get; set; }
