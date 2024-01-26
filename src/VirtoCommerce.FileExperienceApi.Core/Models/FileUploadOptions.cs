bca8ce539554cea5b67a39269f23f0d2055f81e0
artem-dudarev <ad@virtoway.com>
2024-01-26 14:17:53 +0200

    Fix GraphQL playground exception when upload settings are not defined

--- src/VirtoCommerce.FileExperienceApi.Core/Models/FileUploadOptions.cs
+++ src/VirtoCommerce.FileExperienceApi.Core/Models/FileUploadOptions.cs
@@ -6,7 +6,7 @@ namespace VirtoCommerce.FileExperienceApi.Core.Models;
 public class FileUploadOptions
 {
     [Required]
-    public string RootPath { get; set; }
+    public string RootPath { get; set; } = "upload";
 
     [Required]
     public List<FileUploadScopeOptions> Scopes { get; set; } = new();
